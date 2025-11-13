73bac2f4974c6137efde0af20c5aef209c0b508f
Dmitriyxt5 <dimon777526@gmail.com>
2025-11-13 10:29:57 +0500

    images sliders

--- resources/js/Components/Slider.vue
+++ resources/js/Components/Slider.vue
@@ -8,7 +8,7 @@
 			<!-- Слайд 1 -->
 			<SplideSlide class="flex justify-center items-center">
 				<img
-					src="https://kipk.edu.kz/wp-content/uploads/2020/10/IMG_3916-2-1120x600.jpg"
+					src="/images/slider/1.jpeg"
 					alt="Образец 1"
 					class="w-full h-auto max-h-96 object-contain rounded-lg"
 				/>
@@ -17,7 +17,21 @@
 			<!-- Слайд 2 -->
 			<SplideSlide class="flex justify-center items-center">
 				<img
-					src="/images/photo_2025-11-11_19-38-03.jpg"
+					src="/images/slider/2.jpeg"
+					alt="Образец 2"
+					class="w-full h-auto max-h-96 object-contain rounded-lg"
+				/>
+			</SplideSlide>
+			<SplideSlide class="flex justify-center items-center">
+				<img
+					src="/images/slider/3.jpeg"
+					alt="Образец 2"
+					class="w-full h-auto max-h-96 object-contain rounded-lg"
+				/>
+			</SplideSlide>
+			<SplideSlide class="flex justify-center items-center">
+				<img
+					src="/images/slider/4.jpeg"
 					alt="Образец 2"
 					class="w-full h-auto max-h-96 object-contain rounded-lg"
 				/>
